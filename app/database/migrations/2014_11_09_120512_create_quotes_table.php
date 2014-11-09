30935243a3ad1da0dba3001dccb75de281c47368
Tommy Ku <zoo1929@outlook.com>
2014-11-09 20:09:08 +0800

    migration done

--- app/database/migrations/2014_11_09_120512_create_quotes_table.php
+++ app/database/migrations/2014_11_09_120512_create_quotes_table.php
@@ -5,28 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateQuotesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('ol_quotes', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ol_quotes', function(Blueprint $t)
+        {
+            $t->increments('id');
+            $t->string('quote');
+            $t->string('author');
+            $t->string('image');
+            $t->timestamps();
+            $t->softDeletes();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('ol_quotes');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ol_quotes');
+    }
 
 }
