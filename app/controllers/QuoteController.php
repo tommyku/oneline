c39e6a0c73c7a53e6aa4f2b3506e8ee0e9a0a65c
Tommy Ku <zoo1929@outlook.com>
2014-11-09 20:06:17 +0800

    created migration and controller for quotes

---
+++ app/controllers/QuoteController.php
@@ -0,0 +1,39 @@
+<?php
+
+class QuoteController extends \BaseController {
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
